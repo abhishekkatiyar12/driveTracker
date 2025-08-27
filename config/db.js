a0997c280cb7f105595cfc9945c0ee2fa2a3e90c
Abhishek Katiyar <142379104+abhishekkatiyar12@users.noreply.github.com>
2025-08-27 20:31:28 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const DB_URL = process.env.DB_URL;
-console.log("DB_URL from env:", DB_URL);
+//console.log("DB_URL from env:", DB_URL);
 
 
 const connectDB = async () => {
